07529b8c4a1ee630f379b49a3eeb4a367171b131
ahmed.al.lakani@gmail.com <ahmed.al.lakani@gmail.com>
2020-05-11 15:49:30 +0300

    Add project files.

---
+++ NotepadTabs/MainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NotepadTabs
+{
+    public partial class MainWindow : Form
+    {
+        TextBox m_editor[];
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
